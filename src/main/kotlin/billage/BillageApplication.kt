a568e0479251276eb2e39ed8f316d4415bfc27b2
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-12-31 04:42:37 +0900

    initialize project

---
+++ src/main/kotlin/billage/BillageApplication.kt
@@ -0,0 +1,11 @@
+package billage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BillageApplication
+
+fun main(args: Array<String>) {
+	runApplication<BillageApplication>(*args)
+}
